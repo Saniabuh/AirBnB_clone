40cea44f7eff338be2a1139f1a98c3b1e8b2daaf
BlezynUkatu <blessingukatu1@gmail.com>
2022-08-31 04:19:59 -0700

    base models

--- models/base_model.py
+++ models/base_model.py
@@ -34,7 +34,3 @@ print(my_model)
 my_model.save()
 print(my_model)
 my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
