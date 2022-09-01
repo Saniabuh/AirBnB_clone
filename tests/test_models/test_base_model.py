cba76a5ee066b24f71304eff4d2e354b67f62231
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-01 13:09:55 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Defines unittests for models/base_model.py.
 Unittest classes:
-    TestBaseModel_instantiation
-    TestBaseModel_save
-    TestBaseModel_to_dict
 """
+
 import os
-import models
 import unittest
+import models
 from datetime import datetime
 from time import sleep
 from models.base_model import BaseModel
