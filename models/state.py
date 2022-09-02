9991e9a0fdd0e409db681725e4c0588626602cf1
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-02 14:43:56 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Defines the State class."""
+"""This module holds the state class"""
+
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state.
-    Attributes:
-        name (str): The name of the state.
-    """
+    """Implementation of the state class"""
 
     name = ""
