6e04648e5c90725a390949baf7ea30201fc4b714
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-02 15:08:16 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,7 +13,7 @@ Unittest classes:
 import os
 import sys
 import unittest
-from models import storage
+"from models import storage"
 from models.engine.file_storage import FileStorage
 from console import HBNBCommand
 from io import StringIO
