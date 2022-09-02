c88fc701608d9a88697fceff40e5fad33b991f7c
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-02 13:46:00 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-Test BaseModel for expected behavior.
+Test BaseModel for expected behavior and documentation
 """
 from datetime import datetime
 import inspect
-import models
+from models import base_model
 import pep8 as pycodestyle
 import time
 import unittest
 from unittest import mock
-BaseModel = models.base_model.BaseModel
-module_doc = models.base_model.__doc__
+BaseModel = base_model.BaseModel
+module_doc = base_model.__doc__
 
 
 class TestBaseModelDocs(unittest.TestCase):
