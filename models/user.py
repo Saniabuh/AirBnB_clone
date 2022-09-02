bbe8fe068309b9e2eefb1cac5abf709c15bfec3c
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-02 13:48:27 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Defines the User information."""
+"""
+This module contains a class User
+"""
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+    """
+    Implemenatation of the class User
     """
 
     email = ""
