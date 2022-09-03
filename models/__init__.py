df7665bf33d5f936ebe2e91fd581eaa0f6f2b7bd
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-03 23:00:27 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+
 """
-Initializes the models package
+__init__ for model package
 """
 
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models.engine.file_storage import FileStorage
-
 
 storage = FileStorage()
 storage.reload()
