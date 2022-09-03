aa9f5ae52b18c61a2ae254688832f5e441a8f7ca
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-03 20:26:37 +0100

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,69 @@
 #!/usr/bin/python3
-"""
-This module contains a class BaseModel
-"""
-
 from datetime import datetime
 import uuid
-
-date_format = "%Y-%m-%dT%H:%M:%S.%f"
+import models
+"""
+A module that defines a base class
+with all common attributes/method for other classes
+"""
 
 
 class BaseModel():
     """
-    Implementation of BaseModel class
+    A base class for other classes
+    defines all attributes/method
+    all subclasses could inherit from
     """
     def __init__(self, *args, **kwargs):
         """
-        Initialization of the class
+        A class constructor
+        initializes the private instance variables
+        assigns new values to the variables
+        if no kwargs was passed
+        sets the **kwargs if it exists
+        using iteration
+        self.kwargs[key] = value
         """
-        from models import storage
+        self.number = 0
+        self.name = type(self).__name__
+        self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if hasattr(self, "created_at") and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"],
-                                                    date_format)
-            if hasattr(self, "updated_at") and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                    date_format)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
 
-    def __str__(self):
-        """
-        String representation of the class
-        """
-        return ("[{:s}] ({:s}) {}".format(self.__class__.__name__,
-                                          self.id, self.__dict__))
+        else:
+            models.storage.new(self)
 
     def save(self):
+        """A base class that updates the \"self.updated_at\"
+        to the current time when the object was saved
         """
-        updates the public instance attribute updated_at with the
-        current datetime
-        """
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__
-        of the instance
+        A method that returns a dictionary
+        containing all keys/values of __dict__ instance
+        with some added parameters
+        A key __class__ added
+        Created_at and Updated_at:
+            converted to string object in ISO format using .isoformat()
         """
-        rdict = self.__dict__.copy()
-        rdict["__class__"] = self.__class__.__name__
-        rdict["created_at"] = datetime.isoformat(self.created_at)
-        rdict["updated_at"] = datetime.isoformat(self.updated_at)
-        return ndict
+        new_dict = self.__dict__.copy()
+        new_dict['name'] = self.name
+        new_dict['__class__'] = self.name
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['created_at'] = self.created_at.isoformat()
+        return new_dict
+
+    def __str__(self):
+        """String representation of BaseModel instances"""
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
