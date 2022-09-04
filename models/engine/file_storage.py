f8975ac4afcce908bf41745310788609f41d823c
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-04 08:49:38 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,69 +1,69 @@
 #!/usr/bin/python3
+
+"""
+A module that defines a class FileStorage for database engine
+"""
+
 import json
-from models.amenity import Amenity
-from models.base_model import BaseModel
+from models.user import User
 from models.city import City
+from models.state import State
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
+from models.amenity import Amenity
+from models.base_model import BaseModel
 
 
-class FileStorage():
+class FileStorage:
     """
-    deserializes instance of a JSON FILE
-    serializes instance to a JSON FILE
+    Serializes instances to a JSON file and deserializes JSON file to instances
+    Attr:
+        __file_path (str): path to the JSON file
+        __objects (dictionary): empty dictionary,
+        stores object with key as '<class name>.id
     """
-
+    __file_path = "file.json"
     __objects = {}
-    __file_path = 'file.json'
-    __file_path2 = 'storage.json'
 
     def all(self):
-        """Returns the __objects dict"""
-        return type(self).__objects
+        """
+        Return:
+            dictionary (dict): __objects attribute
+        """
+        return self.__objects
 
     def new(self, obj):
-        """Adds obj in the __objects dict"""
-        key = obj.__class__.__name__ + "." + obj.id
-        type(self).__objects[key] = obj
+        """
+        Sets in __objects the obj with key <obj class name>.id
+        Attr:
+            obj (BaseModel): instance obj
+        """
+        key = "{:s}.{:s}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
-    def destroy(self, arg="None"):
-        """deletes the storage"""
-        if arg == "None":
-            type(self).__objects.clear()
-        else:
-            type(self).__objects.pop(arg)
+    def clean(self):
+        """Resets the private __object to an empty dictionary"""
+        self.__objects = {}
 
     def save(self):
-        """serializes __objects to __file_path"""
-        with open(type(self).__file_path, "w", encoding='utf-8') as file:
-            dictionary = {}
-            for key, value in type(self).__objects.items():
-                dictionary[key] = value.to_dict()
-            # type(self).__objects.clear()
-            json.dump(dictionary, file, indent=2)
-        with open(type(self).__file_path2, "w") as file1:
-            dictionary1 = {}
-            for key, value in type(self).__objects.items():
-                dictionary1[key] = value
-            json.dump(dictionary1, file1, indent=2, default=str)
+        """
+        Serializes __objects to the JSON file (path: __file_path)
+        """
+        objs = {k: v.to_dict() for k, v in self.__objects.items()}
+        with open(self.__file_path, mode='w', encoding='utf-8') as j_file:
+            j_file.write(json.dumps(objs))
 
     def reload(self):
         """
-        deserializes the json file to __objects
-        only if the json file exists
-        otherwise do nothing if the file doesn't exists
-        no exception is raised
+        Deserializes the JSON file to __objects (only if the JSON file exits)
         """
         try:
-            with open(self.__file_path, 'r') as file:
-                value = json.load(file)
-                # self.__objects.update(value)
-                # file.close()
-                for object in value.values():
-                    class_name = object['__class__']
-                    del object['__class__']
-                    self.new(eval(class_name)(**object))
-        except (FileNotFoundError, json.decoder.JSONDecodeError):
-            return
+            with open(self.__file_path, mode='r', encoding='utf-8') as j_file:
+                data = j_file.read()
+            if data != "" and data is not None:
+                data = json.loads(data)
+                for value in data.values():
+                    Clazz = eval(value['__class__'])
+                    self.new(Clazz(**value))
+        except FileNotFoundError:
+            pass
