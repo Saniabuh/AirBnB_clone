16bb45fb124960cb9ded099b3ec683d8bb57b487
Sani Abuh <101403891+Saniabuh@users.noreply.github.com>
2022-09-06 20:57:05 +0100

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,93 +1,119 @@
 #!/usr/bin/python3
 """
-This module contains unittests for class City
+This module contains unittests for class Place
 """
 
+from datetime import datetime
 import inspect
-from models import city
+from models import place
 from models.base_model import BaseModel
-import pycodestyle
+import pep8
 import unittest
-City = city.City
+Place = place.Place
 
 
-class TestCityDocs(unittest.TestCase):
-    """Tests to check the documentation and style of City class"""
+class TestUserDocs(unittest.TestCase):
+    """Tests to check the documentation and style of Place class"""
     @classmethod
     def setUpClass(cls):
         """Set up for the doc tests"""
-        cls.city_f = inspect.getmembers(City, inspect.isfunction)
+        cls.place_f = inspect.getmembers(Place, inspect.isfunction)
 
-    def test_pep8_conformance_city(self):
-        """Test that models/city.py conforms to PEP8."""
-        pep8s = pycodestyle.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/city.py'])
+    def test_pep8_conformance_user(self):
+        """Test that models/place.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/place.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_pep8_conformance_test_city(self):
-        """Test that tests/test_models/test_city.py conforms to PEP8."""
-        pep8s = pycodestyle.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_city.py'])
+    def test_pep8_conformance_test_user(self):
+        """Test that tests/test_models/test_place.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_place.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_city_module_docstring(self):
-        """Test for the city.py module docstring"""
-        self.assertIsNot(city.__doc__, None,
-                         "city.py needs a docstring")
-        self.assertTrue(len(city.__doc__) >= 1,
-                        "city.py needs a docstring")
+    def test_user_module_docstring(self):
+        """Test for the amenity.py module docstring"""
+        self.assertIsNot(place.__doc__, None,
+                         "place.py needs a docstring")
+        self.assertTrue(len(place.__doc__) >= 1,
+                        "place.py needs a docstring")
 
-    def test_city_class_docstring(self):
-        """Test for the city class docstring"""
-        self.assertIsNot(City.__doc__, None,
-                         "City class needs a docstring")
-        self.assertTrue(len(City.__doc__) >= 1,
-                        "City class needs a docstring")
+    def test_user_class_docstring(self):
+        """Test for the Amenity class docstring"""
+        self.assertIsNot(Place.__doc__, None,
+                         "Place class needs a docstring")
+        self.assertTrue(len(Place.__doc__) >= 1,
+                        "Place class needs a docstring")
 
-    def test_city_func_docstrings(self):
-        """Test for the presence of docstrings in City methods"""
-        for func in self.city_f:
+    def test_user_func_docstrings(self):
+        """Test for the presence of docstrings in Amenity methods"""
+        for func in self.place_f:
             self.assertIsNot(func[1].__doc__, None,
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
 
-class TestCity(unittest.TestCase):
+class TestAmenity(unittest.TestCase):
     """
-    Tests the functionality of the city class
+    Tests the functionality of the place class
     """
 
     def test_Inheritance(self):
         """
-        Tests that city is a subclass of BaseModel
+        Tests that place is a subclass of BaseModel
         """
-        city = City()
-        self.assertIsInstance(city, BaseModel)
-        self.assertTrue(hasattr(city, "id"))
-        self.assertTrue(hasattr(city, "created_at"))
-        self.assertTrue(hasattr(city, "updated_at"))
+        place = Place()
+        self.assertIsInstance(place, BaseModel)
+        self.assertTrue(hasattr(place, "id"))
+        self.assertTrue(hasattr(place, "created_at"))
+        self.assertTrue(hasattr(place, "updated_at"))
 
     def test_Attributes(self):
         """
         Tests that the class has the correct attributes
         """
-        city = City()
-        self.assertTrue(hasattr(city, "name"))
-        self.assertEqual(city.name, "")
-        self.assertTrue(hasattr(city, "state_id"))
-        self.assertEqual(city.state_id, "")
+        place = Place()
+        self.assertTrue(hasattr(place, "city_id"))
+        self.assertEqual(place.city_id, "")
+        self.assertTrue(hasattr(place, "user_id"))
+        self.assertEqual(place.user_id, "")
+        self.assertTrue(hasattr(place, "name"))
+        self.assertEqual(place.name, "")
+        self.assertTrue(hasattr(place, "description"))
+        self.assertEqual(place.description, "")
+        self.assertTrue(hasattr(place, "number_rooms"))
+        self.assertEqual(place.number_rooms, 0)
+        self.assertTrue(hasattr(place, "number_bathrooms"))
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertTrue(hasattr(place, "max_guest"))
+        self.assertEqual(place.max_guest, 0)
+        self.assertTrue(hasattr(place, "price_by_night"))
+        self.assertEqual(place.price_by_night, 0)
+        self.assertTrue(hasattr(place, "latitude"))
+        self.assertEqual(place.latitude, 0.0)
+        self.assertTrue(hasattr(place, "longitude"))
+        self.assertEqual(place.longitude, 0.0)
+        self.assertTrue(hasattr(place, "amenity_ids"))
+        self.assertEqual(place.amenity_ids, [])
+        self.assertEqual(type(place.number_rooms), int)
+        self.assertEqual(type(place.number_bathrooms), int)
+        self.assertEqual(type(place.max_guest), int)
+        self.assertEqual(type(place.price_by_night), int)
+        self.assertEqual(type(place.latitude), float)
+        self.assertEqual(type(place.longitude), float)
+        self.assertEqual(type(place.amenity_ids), list)
 
     def test_to_dict(self):
         """
-        Tests that to_dict method works with City
+        Tests that to_dict method works with the class
         """
-        ci = City()
-        ndict = ci.to_dict()
+        pl = Place()
+        ndict = pl.to_dict()
         self.assertEqual(type(ndict), dict)
-        for attr in ci.__dict__:
+        for attr in pl.__dict__:
             self.assertTrue(attr in ndict)
             self.assertTrue("__class__" in ndict)
 
@@ -96,18 +122,18 @@ class TestCity(unittest.TestCase):
         Tests that dictionary has correct values
         """
         isoforma = "%Y-%m-%dT%H:%M:%S.%f"
-        ci = City()
-        new_d = ci.to_dict()
-        self.assertEqual(new_d["__class__"], "City")
+        pl = Place()
+        new_d = pl.to_dict()
+        self.assertEqual(new_d["__class__"], "Place")
         self.assertEqual(type(new_d["created_at"]), str)
         self.assertEqual(type(new_d["updated_at"]), str)
-        self.assertEqual(new_d["created_at"], ci.created_at.strftime(isoforma))
-        self.assertEqual(new_d["updated_at"], ci.updated_at.strftime(isoforma))
+        self.assertEqual(new_d["created_at"], pl.created_at.strftime(isoforma))
+        self.assertEqual(new_d["updated_at"], pl.updated_at.strftime(isoforma))
 
     def test_str(self):
         """
         Tests that str method returns correct output
         """
-        city = City()
-        string = "[City] ({}) {}".format(city.id, city.__dict__)
-        self.assertEqual(string, str(city))
+        place = Place()
+        string = "[Place] ({}) {}".format(place.id, place.__dict__)
+        self.assertEqual(string, str(place))
